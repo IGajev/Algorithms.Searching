460b9ec447d8160fd45d297d5c2b3efcff768e5c
ivangajev <ivan.gajev@paysafe.com>
2020-10-15 18:52:14 +0300

    Add FrequencyCounter client.

---
+++ Symbol Tables/src/main/java/org/symbol/table/ST.java
@@ -0,0 +1,20 @@
+package org.symbol.table;
+
+public class ST<T, T1> {
+
+  public boolean contains(T key) {
+    return false;
+  }
+
+  public void put(T key, T1 value) {
+
+  }
+
+  public T1 get(T key) {
+    return null;
+  }
+
+  public String[] keys() {
+    return null;
+  }
+}
